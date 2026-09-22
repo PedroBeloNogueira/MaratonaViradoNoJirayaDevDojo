8c67a157f6899974c56baa907d813636330a75cd
Dokas006 <belop065@gmail.com>
2026-09-22 08:16:34 -0300

    Set,HasSet and Iterator

--- src/Colecoes/Test/SetTest.java
+++ src/Colecoes/Test/SetTest.java
@@ -2,11 +2,12 @@ package Colecoes.Test;
 
 import Colecoes.Dominio.Manga;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Iterator {
+public class SetTest {
     static void main() {
-        List<Manga> mangas = new LinkedList<>();
+        Set<Manga> mangas = new HashSet<Manga>();
         mangas.add(new Manga(2, "Naruto", 1));
         mangas.add(new Manga(5, "Dan da Dan", 2));
         mangas.add(new Manga(3, "Dragon Ball Z",5));
@@ -14,13 +15,10 @@ public class Iterator {
         mangas.add(new Manga(4, "Olhos de Gato",0));
         mangas.add(new Manga(0, "Suzume ", 0));
 
-        java.util.Iterator<Manga> iterator = mangas.iterator();
-        while (iterator.hasNext()) {
-            Manga manga = iterator.next();
-           if (manga.getQuantidade() == 0)
-               iterator.remove();
+
+        for (Manga manga : mangas) {
+            System.out.println(manga);
         }
-        Collections.sort(mangas);
-        System.out.println(mangas);
+
     }
 }
